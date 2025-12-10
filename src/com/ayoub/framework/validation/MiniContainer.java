0097e7d482d41eaf6e95c1c2ad337cef65a17372
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-12-10 16:26:00 +0100

    ref: added sigleton , cache

--- src/com/ayoub/framework/validation/MiniContainer.java
+++ src/com/ayoub/framework/validation/MiniContainer.java
@@ -1,22 +1,34 @@
 package com.ayoub.framework.validation;
 
 import java.lang.reflect.Constructor;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MiniContainer {
     BeanDefinition config = new BeanDefinition();
+    Map<Class<?>, Object> singletonObjects = new HashMap<>();
 
     public Object getBean(Class<?> interfaceClass) {
-        Class<?> myClass = config.beanMapping.get(interfaceClass); // so when we did map.get(key) we get the value: in this case it's paypal
 
-        if (myClass == null) {
-            throw new RuntimeException("Bean not found!");
+        //check the cache first
+        if (singletonObjects.containsKey(interfaceClass)){
+            return singletonObjects.get(interfaceClass);
         }
+
+        Class<?> implClass = config.beanMapping.get(interfaceClass);
+
+        if (implClass == null) {
+            throw new RuntimeException("No bean definition for: " + interfaceClass.getName());
+        }
+
+        // create the instance
         Object object;
         try {
-            Constructor<?> constructor = myClass.getDeclaredConstructor();
+            Constructor<?> constructor = implClass.getDeclaredConstructor();
             object = constructor.newInstance();
+            singletonObjects.put(interfaceClass, object);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to create bean: " + implClass.getName(), e);
         }
         return object;
     }
